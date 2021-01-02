a011bae0be6836c4117157ef381a04c62b3e7694
plotrekhq <plotrekhqpl@gmail.com>
2021-01-02 20:31:44 +0100

    add styled-components

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -3,6 +3,37 @@ import React, { useState } from "react";
 import "../../app/layout/styles.css";
 import { MenuOutlined } from "@ant-design/icons";
 import logo from "../../app/assets/logo1.png";
+import Search from "antd/lib/input/Search";
+import styled from "styled-components";
+
+const StyledSearch = styled(Search)`
+  .ant-input {
+    background-color: transparent;
+    border: none;
+    font-size: 15px;
+  }
+
+  .ant-input::placeholder {
+    color: black;
+  }
+  .ant-btn {
+    background-color: transparent;
+    border:none;
+    width:31px;
+    height:31px;
+  }
+  .ant-btn:hover {
+    border: none;
+  }
+  .ant-input-group-addon {
+    background-color: transparent;
+    border: none;
+
+  }
+  .anticon .anticon-search{
+    color: #000;
+  }
+`;
 
 export default function NavBar() {
   const [visible, setVisible] = useState(false);
@@ -20,7 +51,7 @@ export default function NavBar() {
           <a href="/">S Tech</a>
         </div>
         <div>
-          <Input.Search  size="large" placeholder="input here" />
+          <StyledSearch placeholder="Wyszukaj w sklepie" />
         </div>
         <div className="mobileHidden">
           <Menu mode="horizontal">
